2a5a7962cab9a47150bc21ae05bf102992a7eacd
anhp95 <anhph95@gmail.com>
2022-08-19 16:59:54 +0900

    update CORS

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 #%%
 import flask
-from flask import request
 
-import geopandas as gpd
-import pandas as pd
+from flask import request, jsonify
 from api_emission import emission_by_year
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def get_emission():
+    
     year = int(request.args['year'])
-
-    return emission_by_year(year)
-
-
-# %%
+    return jsonify(emission_by_year(year))
