55db5ece2dc20ed6bf0a72d8bfeeffc53117e263
anhp95 <anhph95@gmail.com>
2022-08-25 11:33:33 +0900

    fix port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #%%
 import flask
+import os
 
 from modules.ems_tracker import bp
 from flask_cors import CORS
@@ -8,6 +9,9 @@ app = flask.Flask(__name__)
 CORS(app)
 
 app.register_blueprint(bp, url_prefix="/ems_tracker")
-app.run(debug=True, port=5001)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 # %%
