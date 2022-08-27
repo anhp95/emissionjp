a64ffdf41450ef74f1245b8d4619f002148d5ffd
anhp95 <anhph95@gmail.com>
2022-08-27 22:32:27 +0900

    update zeroems api

---
+++ core/zero_ems/api_zero_ems.py
@@ -0,0 +1,43 @@
+#%%
+from email.mime import base
+import pandas as pd
+import os
+
+BASE_DIR = "data/zeroemission"
+
+
+def read_csv(file_name):
+
+    df = pd.read_csv(os.path.join(BASE_DIR, file_name))
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+
+    return df
+
+
+def get_all_data():
+
+    overall_df = read_csv("overall.csv")
+    fig1_df = read_csv("fig1.csv")
+    fig2_df = read_csv("fig2.csv")
+    fig3_df = read_csv("fig3.csv")
+    fig4_df = read_csv("fig4.csv")
+    fig5_df = read_csv("fig5.csv")
+
+    return overall_df, fig1_df, fig2_df, fig3_df, fig4_df, fig5_df
+
+
+def get_data_by_commune_code(
+    commune_code, overall_df, fig1_df, fig2_df, fig3_df, fig4_df, fig5_df
+):
+
+    return {
+        # "overall": overall_df[overall_df["commune_code"] == commune_code].to_dict()[0],
+        "fig1": fig1_df[fig1_df["commune_code"] == commune_code].to_dict(),
+        "fig2": fig2_df[fig2_df["commune_code"] == commune_code].to_dict(),
+        "fig3": fig3_df[fig3_df["commune_code"] == commune_code].to_dict(),
+        "fig4": fig4_df[fig4_df["commune_code"] == commune_code].to_dict(),
+        "fig5": fig5_df[fig5_df["commune_code"] == commune_code].to_dict(),
+    }
+
+
+# %%
--- modules/zero_ems/zero_ems.py
+++ modules/zero_ems/zero_ems.py
@@ -2,17 +2,30 @@
 from flask_restx import Namespace, Resource
 from flask import request, jsonify
 
+from core.zero_ems.api_zero_ems import get_all_data, get_data_by_commune_code
+
+
 api_zero_ems = Namespace("zero_ems", description="Zero Emission scenario")
 
+OVERALL_DF, FIG1_DF, FIG2_DF, FIG3_DF, FIG4_DF, FIG5_DF = get_all_data()
+
+
 @api_zero_ems.route("/")
-@api_zero_ems.param("commune", "A commune of Japan")
+@api_zero_ems.param("commune_code", "A commune of Japan")
 class ZeroEms(Resource):
     """
     Return Overall Emission at commune level by year
     """
-    @api_zero_ems.doc(commune="E.g., Nagoya")
+
+    @api_zero_ems.doc(commune_code="Commune code according to Excel tool")
     def get(self):
-        commune = request.args.get("commune")
-        return {"commune": commune}
+
+        commune_code = request.args.get("commune_code")
+        zero_ems_data = get_data_by_commune_code(
+            commune_code, OVERALL_DF, FIG1_DF, FIG2_DF, FIG3_DF, FIG4_DF, FIG5_DF
+        )
+
+        return zero_ems_data
+
 
 # %%
